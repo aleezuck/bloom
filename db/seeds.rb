a77192d6e76bb414bbab86bfc1855c3203ab2b9e
Amanda Lee-Zuck <a.leezuck@gmail.com>
2021-11-23 11:06:05 -0500

    Added some starting seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,65 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding plants..."
+
+Plant.destroy_all
+
+Plant.create!(
+  name: "Monstera deliciosa",
+  description: "Monstera deliciosa plant is also known as the \"split-leaf philodendron.\"",
+  category: "tropical",
+  toxic: true,
+  light_level: 4,
+  price: 40,
+  care_level: "low",
+)
+
+Plant.create!(
+  name: "Spider plant",
+  description: "Spider plants clean the air of harmful toxins.",
+  category: "tropical",
+  toxic: false,
+  light_level: 2,
+  price: 20,
+  care_level: "low",
+)
+
+Plant.create!(
+  name: "African violet",
+  description: "Flowers may be pink, blue, purple, or white.",
+  category: "flowering",
+  toxic: false,
+  light_level: 3,
+  price: 30,
+  care_level: "medium",
+)
+
+Plant.create!(
+  name: "Lucky bamboo",
+  description: "Lucky bamboo ceases to grow when you clip the top, making it easier to manage.",
+  category: "bamboo",
+  toxic: false,
+  light_level: 1,
+  price: 60,
+  care_level: "high",
+)
+
+Plant.create!(
+  name: "Basil",
+  description: "Trim off flowers to keep those fragrant leaves coming.",
+  category: "herb",
+  toxic: false,
+  light_level: 5,
+  price: 10,
+  care_level: "high",
+)
+
+Plant.create!(
+  name: "Heartleaf philodendron",
+  description: "Whether trailing from a basket or growing up a trellis, Philodendron scandens is the classic low-care houseplant that thrives in all light conditions.",
+  category: "hanging",
+  toxic: true,
+  light_level: 2,
+  price: 30,
+  care_level: "low",
+)
+
+puts "Finished seed! :)"
