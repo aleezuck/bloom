66debe2a152027289c25dcaed264954dc75b7d66
Amanda Lee-Zuck <a.leezuck@gmail.com>
2021-11-24 16:27:32 -0500

    Added AJAX search with Stimulus.

--- app/javascript/controllers/plant_list_controller.js
+++ app/javascript/controllers/plant_list_controller.js
@@ -1,9 +1,43 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  // static targets = [ "form", "list", "searchInput" ]
+  static targets = [ "form", "list", "nameInput", "priceInput", "petInput", "lightInput", "careInput" ]
 
   connect() {
-    console.log("connected");
+    // console.log(this.petInputTargets);
+  }
+
+  update() {
+    let pet = "";
+    for (const petInput of this.petInputTargets) {
+      if (petInput.checked) {
+        pet = petInput.value;
+        break;
+      }
+    }
+
+    let light = "";
+    for (const lightInput of this.lightInputTargets) {
+      if (lightInput.checked) {
+        light = lightInput.value;
+        break;
+      }
+    }
+
+    let care = "";
+    for (const careInput of this.careInputTargets) {
+      if (careInput.checked) {
+        care = careInput.value;
+        break;
+      }
+    }
+
+    const url = `${this.formTarget.action}?query=${this.nameInputTarget.value}&max_price=${this.priceInputTarget.value}&pets=${pet}&light_level=${light}&care_level=${care}`;
+
+    fetch(url, { headers: { 'Accept': 'text/plain' } })
+      .then(response => response.text())
+      .then((data) => {
+        this.listTarget.outerHTML = data;
+      })
   }
 }
