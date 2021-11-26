393715de6df6b809ec557ab471dcdbb728a1b847
Amanda Lee-Zuck <a.leezuck@gmail.com>
2021-11-26 12:24:40 -0500

    Added clear button for each option.

--- app/javascript/controllers/plant_form_controller.js
+++ app/javascript/controllers/plant_form_controller.js
@@ -1,8 +1,24 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+  static targets = []
 
   connect() {
-    // console.log('hi');
+    // console.log("connected");
+  }
+
+  clearPets(event) {
+    event.preventDefault();
+    document.getElementsByName("pets").forEach(element => element.checked = false)
+  }
+
+  clearLight(event) {
+    event.preventDefault();
+    document.getElementsByName("light_level").forEach(element => element.checked = false)
+  }
+
+  clearCare(event) {
+    event.preventDefault();
+    document.getElementsByName("care_level").forEach(element => element.checked = false)
   }
 }
