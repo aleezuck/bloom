35c77aae5f7e72420012f5f3704924a6a656d3d0
Amanda Lee-Zuck <a.leezuck@gmail.com>
2021-11-29 14:07:54 -0500

    Added create method for posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,32 @@
 class PostsController < ApplicationController
+  before_action :set_plant, only: [:index, :create]
+
   def index
-    @plant = Plant.find(params[:plant_id])
     @posts = policy_scope(Post.where(plant: @plant))
     @post = Post.new
+  end
+
+  def create
+    @posts = policy_scope(Post.where(plant: @plant))
+    @post = Post.new(post_params)
+    @post.user = current_user
+    @post.plant = @plant
+    authorize(@post)
+
+    if @post.save
+      redirect_to plant_posts_path(@plant)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def set_plant
+    @plant = Plant.find(params[:plant_id])
   end
 end
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -4,4 +4,8 @@ class PostPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def create?
+    true
+  end
 end
