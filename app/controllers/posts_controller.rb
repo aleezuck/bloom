ea795558bed4e26ec1458d4f493d821b775c8ded
Amanda Lee-Zuck <a.leezuck@gmail.com>
2021-11-29 12:06:30 -0500

    Started implementing create method for posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,6 @@ class PostsController < ApplicationController
   def index
     @plant = Plant.find(params[:plant_id])
     @posts = policy_scope(Post.where(plant: @plant))
+    @post = Post.new
   end
 end
