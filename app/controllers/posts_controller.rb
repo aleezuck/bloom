e8b67f13424cce5be57c112ff8f7389eeef534f2
Amanda Lee-Zuck <a.leezuck@gmail.com>
2021-11-29 11:37:03 -0500

    Added basic index page for a plant's forum.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,6 @@
 class PostsController < ApplicationController
+  def index
+    @plant = Plant.find(params[:plant_id])
+    @posts = policy_scope(Post.where(plant: @plant))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :plants, only: [:index, :show] do
     resources :wishlist_items, only: [:create]
+    resources :posts, only: [:index, :create]
   end
 
   get '/my_wishlist_items', to: 'wishlist_items#my_wishlist_items', as: :my_wishlist_items
