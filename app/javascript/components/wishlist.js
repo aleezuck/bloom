d49dbb18630c8c072da75901e081091f7efc210a
Amanda Lee-Zuck <a.leezuck@gmail.com>
2021-11-30 16:06:44 -0500

    Added info bubble to landing page form.

--- app/javascript/components/wishlist.js
+++ app/javascript/components/wishlist.js
@@ -1,5 +1,5 @@
 const changeColor = () => {
-  const color = document.querySelector(".far");
+  const color = document.querySelector(".wishlist-heart");
   if (color) {
     color.addEventListener("click", (event) => {
       event.currentTarget.classList.toggle("fa-heart-black");
